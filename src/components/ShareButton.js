6c2748d53f195542bab3543c9e04e8c8406decc0
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-10-18 11:51:41 +0200

    Fix linting warning

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -28,9 +28,9 @@ const ShareButton = ({
   const link = getShareLink(
     attribution,
     canvasLink,
-    label,
     provider,
-    thumbnailUrl
+    thumbnailUrl,
+    label
   );
   const ProviderIcon = iconMapping[provider];
   return (
